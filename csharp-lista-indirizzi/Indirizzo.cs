1a039ce19379c5b26d9f9670c2959a6243051ebc
diegoG82 <diegogiardina@hotmail.it>
2023-09-14 14:08:41 +0200

    Aggiungere i file di progetto.

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,30 @@
+﻿namespace csharp_lista_indirizzi
+
+
+{
+    public class Indirizzo
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public int Zip { get; set; }
+
+
+        public Indirizzo(string street, string city, string province, int zip)
+        {
+            Street = street;
+            City = city;
+            Province = province;
+            Zip = zip;
+        }
+
+
+    }
+}
+
+
---
+++ csharp-lista-indirizzi/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+
+using csharp_lista_indirizzi;
+
+List<Indirizzo> indirizzi = new List<Indirizzo>();
